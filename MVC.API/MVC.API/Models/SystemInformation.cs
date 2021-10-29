530377378e50d76956e89084df15a92cebbac10b
shrivadan <shrivadank@cybage.com>
2021-10-29 10:16:18 +0530

    API done

---
+++ MVC.API/MVC.API/Models/SystemInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace WebApiSystemInformation.Models
+{
+    public class SystemInformation
+    {
+        public String MachineName
+        {
+            get;
+            set;
+        }
+        public String OsName
+        {
+            get;
+            set;
+        }
+        public String IPAddress
+        {
+            get;
+            set;
+        }
+    }
+}
